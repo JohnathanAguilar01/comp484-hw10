82c8cb0c244370b4b97fdcd4d44d8f4eecbf8751
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2025-11-16 00:34:48 -0800

    finished a whole lot

--- script.js
+++ script.js
@@ -1,52 +1,155 @@
-$(function() { // Makes sure that your function is called once all the DOM elements of the page are ready to be used.
-    
-    // Called function to update the name, happiness, and weight of our pet in our HTML
-    checkAndUpdatePetInfoInHtml();
-  
-    // When each button is clicked, it will "call" function for that button (functions are below)
-    $('.treat-button').click(clickedTreatButton);
-    $('.play-button').click(clickedPlayButton);
-    $('.exercise-button').click(clickedExerciseButton);
-  
+$(function () {
+  $(".create-pet").click(addPet);
 
-  
-    
-  })
-  
-    // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
-    var pet_info = {name:"My Pet Name", weight:"??", happiness:"??"};
-  
-    function clickedTreatButton() {
-      // Increase pet happiness
-      // Increase pet weight
-      checkAndUpdatePetInfoInHtml();
-    }
-    
-    function clickedPlayButton() {
-      // Increase pet happiness
-      // Decrease pet weight
-      checkAndUpdatePetInfoInHtml();
-    }
-    
-    function clickedExerciseButton() {
-      // Decrease pet happiness
-      // Decrease pet weight
-      checkAndUpdatePetInfoInHtml();
-    }
-  
-    function checkAndUpdatePetInfoInHtml() {
-      checkWeightAndHappinessBeforeUpdating();  
-      updatePetInfoInHtml();
-    }
-    
-    function checkWeightAndHappinessBeforeUpdating() {
-      // Add conditional so if weight is lower than zero.
-    }
-    
-    // Updates your HTML with the current values in your pet_info object
-    function updatePetInfoInHtml() {
-      $('.name').text(pet_info['name']);
-      $('.weight').text(pet_info['weight']);
-      $('.happiness').text(pet_info['happiness']);
-    }
-  
+  // open modal
+  $("#adopt-button").on("click", function () {
+    $("#myModal").fadeIn(200);
+  });
+
+  // close modal (X button)
+  $(".close").on("click", function () {
+    $("#myModal").fadeOut(200);
+    $("#modal-inner-content").empty();
+  });
+});
+
+function Pet(petName) {
+  this.name = petName;
+  this.weight = 5;
+  this.happiness = 5;
+
+  this.clickedTreatButton = function () {
+    // Increase pet happiness
+    this.happiness += 1;
+    // Increase pet weight
+    this.weight += 1;
+  };
+
+  this.clickedPlayButton = function () {
+    // Increase pet happiness
+    this.happiness += 1;
+    // Decrease pet weight
+    this.weight -= 1;
+  };
+
+  this.clickedExerciseButton = function () {
+    // Decrease pet happiness
+    this.happiness -= 1;
+    // Decrease pet weight
+    this.weight -= 1;
+  };
+}
+
+function renderPetCard(pet) {
+  // HTMl for each pet card
+  const card = $(`
+    <div class="pet-card">
+      <section class="pet-image-container">
+        <img
+          class="pet-image"
+          src="https://cdn.glitch.com/3aa98e05-3216-497c-a251-210ae4713a83%2Fhound.jpg?1541715339220"
+        />
+      </section>
+      <section class="dashboard">
+        <div>
+          Name: <strong><span class="name">${pet.name}</span></strong>
+        </div>
+        <div>
+          Weight: <strong><span class="weight">${pet.weight}</span> pounds</strong>
+        </div>
+        <div>
+          Happiness:
+          <strong><span class="happiness">${pet.happiness}</span> tail wags (per min)</strong>
+        </div>
+        <div class="button-container">
+          <button class="treat-button">Treat</button>
+          <button class="play-button">Play</button>
+          <button class="exercise-button">Exercise</button>
+        </div>
+      </section>
+    </div>
+  `);
+
+  // Adds the pet object to the #pets section in the html
+  $("#pets").append(card);
+
+  // Event listener for when treat button is clicked
+  card.find(".treat-button").on("click", function () {
+    pet.clickedTreatButton();
+    card.find(".weight").text(pet.weight);
+    card.find(".happiness").text(pet.happiness);
+  });
+
+  // Event listener for when play button is clicked
+  card.find(".play-button").on("click", function () {
+    pet.clickedPlayButton();
+    card.find(".weight").text(pet.weight);
+    card.find(".happiness").text(pet.happiness);
+  });
+
+  // Event listener for when exercise button is clicked
+  card.find(".exercise-button").on("click", function () {
+    pet.clickedExerciseButton();
+    card.find(".weight").text(pet.weight);
+    card.find(".happiness").text(pet.happiness);
+  });
+}
+
+function addPet() {
+  var petName = null;
+  var petType = null;
+
+  const adoptionForm = $(`
+    <form id="adoptionForm">
+      <h2>Adoption Form</h2>
+      
+      <div class="form-name">
+        <strong><label for="name">Name:</label></strong>
+        <input type="text" id="name" name="name" required>
+      </div>
+
+      <div class="pet-choices">
+        <button type="button" id="cat" class="pet-button">
+          <img src="images/cat.png" alt="Button" style="height:120px;">
+        </button>
+        <button type="button" id="dog" class="pet-button">
+          <img src="images/dog.png" alt="Button" style="height:120px;">
+        </button>
+        <button type="button" id="horse" class="pet-button">
+          <img src="images/horse.png" alt="Button" style="height:120px;">
+        </button>
+        <button type="button" id="duck" class="pet-button">
+          <img src="images/duck.png" alt="Button" style="height:120px;">
+        </button>
+      </div>
+      <button type="submit" class="create-pet">Adopt</button>
+    </form>
+  `);
+
+  $("#modal-inner-content").append(adoptionForm);
+
+  adoptionForm.find("#cat").on("click", function () {
+    petType = "cat";
+  });
+
+  adoptionForm.find("#dog").on("click", function () {
+    petType = "dog";
+  });
+
+  adoptionForm.find("#horse").on("click", function () {
+    petType = "horse";
+  });
+
+  adoptionForm.find("#duck").on("click", function () {
+    petType = "duck";
+  });
+
+  adoptionForm.on("submit", function () {
+    petName = $("#name").val();
+
+    let pet = new Pet(petName);
+    renderPetCard(pet);
+    $("#myModal").fadeOut(200);
+    $("#modal-inner-content").empty();
+  });
+}
